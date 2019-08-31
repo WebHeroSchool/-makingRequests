574ea53f2d6221be9b51a79c49c6091c65241f82
Ксения Бурнашова <ksuhablondo@gmail.com>
2019-08-31 13:42:11 +0300

    reindentation

--- script.js
+++ script.js
@@ -27,53 +27,52 @@ Promise.all([dataRequest, getNowDate])
   })
   .then(res => requestInfo.json())
   .then(showUserInfo => {
-
     let userLogin = showUserInfo.login;
- 	  let userPhoto = showUserInfo.avatar_url;
- 	  let userDescription = showUserInfo.bio;
- 	  let userUrl = showUserInfo.html_url;
+    let userPhoto = showUserInfo.avatar_url;
+    let userDescription = showUserInfo.bio;
+    let userUrl = showUserInfo.html_url;
 
- 	let addUserLogin = () => {
- 	  let elementForLogin = document.createElement('h1');
- 	  elementForLogin.innerHTML = userLogin;
- 	  body.appendChild(elementForLogin);
- 	}
+    let addUserLogin = () => {
+      let elementForLogin = document.createElement('h1');
+      elementForLogin.innerHTML = userLogin;
+      body.appendChild(elementForLogin);
+    }
 
- 	let addUserInfo = () => {
- 	  let elemenForInfo = document.createElement('p');
- 	  elemenForInfo.innerHTML = userDescription;
- 	  body.appendChild(elemenForInfo);
- 	}
+    let addUserInfo = () => {
+      let elemenForInfo = document.createElement('p');
+      elemenForInfo.innerHTML = userDescription;
+      body.appendChild(elemenForInfo);
+    }
 
- 	let addUserPhoto = () => {
- 	  let elementForPhoto = document.createElement('img');
- 	  let newString = document.createElement('br');
- 	  elementForPhoto.src = userPhoto
- 	  body.appendChild(newString);
- 	  body.appendChild(elementForPhoto);
- 	}
+    let addUserPhoto = () => {
+      let elementForPhoto = document.createElement('img');
+      let newString = document.createElement('br');
+      elementForPhoto.src = userPhoto
+      body.appendChild(newString);
+      body.appendChild(elementForPhoto);
+    }
 
- 	let addUserUrl = () => {
- 	  let elementForUrl = document.createElement('a');
- 	  let text = document.createTextNode('Profile');
- 	  let newString = document.createElement('br');
- 	  elementForUrl.href = userUrl;
- 	  elementForUrl.appendChild(text);
- 	  body.appendChild(newString);
- 	  body.appendChild(elementForUrl);
- 	}
+    let addUserUrl = () => {
+      let elementForUrl = document.createElement('a');
+      let text = document.createTextNode('Profile');
+      let newString = document.createElement('br');
+      elementForUrl.href = userUrl;
+      elementForUrl.appendChild(text);
+      body.appendChild(newString);
+      body.appendChild(elementForUrl);
+    }
 
- 	let addNowDate = () => {
- 	  let elementForDate = document.createElement('p');
- 	  elementForDate.innerHTML = requestDate;
- 	  body.appendChild(elementForDate);
- 	}
+    let addNowDate = () => {
+      let elementForDate = document.createElement('p');
+      elementForDate.innerHTML = requestDate;
+      body.appendChild(elementForDate);
+    }
 
- 	preloader.style.display = 'none';
- 	addUserLogin();
- 	addUserInfo();
- 	addUserPhoto();
- 	addUserUrl();
- 	addNowDate();
+    preloader.style.display = 'none';
+    addUserLogin();
+    addUserInfo();
+    addUserPhoto();
+    addUserUrl();
+    addNowDate();
   })
   .catch(err => alert(err + 'Информация о пользователе не доступна'));
